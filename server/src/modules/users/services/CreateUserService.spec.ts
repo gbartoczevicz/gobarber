f3467f71e9daaad400bfc9e76e83f1f5260bd017
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-05-16 16:01:33 -0300

    feat(User): add new hash provider to test

--- server/src/modules/users/services/CreateUserService.spec.ts
+++ server/src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,19 @@
 import CreateUserService from '@modules/users/services/CreateUserService';
+
 import UsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 
 import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new UsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'Lorem Ipsum',
@@ -20,8 +26,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with email that already exists', async () => {
     const fakeUsersRepository = new UsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const userEmail = 'email@email.com';
 
