bcc2c627d071a68c915d06543424c47849e84be4
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-05-30 13:18:31 -0300

    feat(User): create UpdateProfile and its tests
    should be able to update profile
    
    should be able to update password
    
    should not be able to update profile when user does not exists
    
    should not be able to change email with an another that already exists
    
    should not be able to update password when current password is not provided
    
    should not be able to update password when current password provided is diffrent from user current password

---
+++ server/src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,133 @@
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+
+import AppError from '@shared/errors/AppError';
+
+let updateProfile: UpdateProfileService;
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    updateProfile = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able to update profile', async () => {
+    const save = jest.spyOn(fakeUsersRepository, 'save');
+
+    const user = await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      email: 'new+lorem@ipsum.dolor',
+      name: 'Lorem Ipsum da Silva',
+    });
+
+    expect(save).toHaveBeenCalledWith(updatedUser);
+
+    expect(updatedUser.email).toBe('new+lorem@ipsum.dolor');
+    expect(updatedUser.name).toBe('Lorem Ipsum da Silva');
+  });
+
+  it('should be able to update password', async () => {
+    const save = jest.spyOn(fakeUsersRepository, 'save');
+
+    const user = await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      email: 'new+lorem@ipsum.dolor',
+      name: 'Lorem Ipsum da Silva',
+      password: 'updated_password',
+      current_password: user.password,
+    });
+
+    expect(save).toHaveBeenCalledWith(updatedUser);
+
+    expect(updatedUser.email).toBe('new+lorem@ipsum.dolor');
+    expect(updatedUser.name).toBe('Lorem Ipsum da Silva');
+    expect(updatedUser.password).toBe('updated_password');
+  });
+
+  it('should not be able to update profile when user does not exists', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'user-non-exists',
+        email: 'new+lorem@ipsum.dolor',
+        name: 'Lorem Ipsum da Silva',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to change email with an another that already exists', async () => {
+    await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem2@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        email: 'lorem@ipsum.dolor',
+        name: 'Lorem Ipsum da Silva',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update password when current password is not provided', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        email: 'new+lorem@ipsum.dolor',
+        name: 'Lorem Ipsum da Silva',
+        password: 'updated_password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update password when current password provided is diffrent from user current password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Lorem Ipsum',
+      email: 'lorem@ipsum.dolor',
+      password: 'sit_amet',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        email: 'new+lorem@ipsum.dolor',
+        name: 'Lorem Ipsum da Silva',
+        password: 'updated_password',
+        current_password: 'sit_ame',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ server/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,84 @@
+import { inject, injectable } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  current_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  private usersRepository: IUsersRepository;
+
+  private hashProvider: IHashProvider;
+
+  constructor(
+    @inject('UsersRepository') usersRepository: IUsersRepository,
+    @inject('HashProvider') hashProvider: IHashProvider,
+  ) {
+    this.usersRepository = usersRepository;
+    this.hashProvider = hashProvider;
+  }
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    current_password,
+    password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    if (email) {
+      const doesEmailAlreadyExists = await this.usersRepository.findByEmail(
+        email,
+      );
+
+      if (doesEmailAlreadyExists && doesEmailAlreadyExists.id !== user_id) {
+        throw new AppError('E-mail already exists');
+      }
+
+      user.email = email;
+    }
+
+    if (password) {
+      if (!current_password) {
+        throw new AppError('Old password must be provided');
+      }
+
+      const checkPassword = await this.hashProvider.compare(
+        current_password,
+        user.password,
+      );
+
+      if (!checkPassword) {
+        throw new AppError('Old password does not match');
+      }
+
+      user.password = await this.hashProvider.generate(password);
+    }
+
+    if (name) {
+      user.name = name;
+    }
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateProfileService;
