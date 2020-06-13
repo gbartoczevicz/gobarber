ef0bcc3cbc76fbbd939be259afabc63e9f8b003f
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-06-13 14:01:10 -0300

    feat(mobile): eslint and prettier

---
+++ mobile/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Go Barber!</Text>
+    </View>
+  );
+};
+
+export default App;
