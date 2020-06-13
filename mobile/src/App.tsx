c3bb96cc764e1eb5d87e346d8c21b2eb3309f8fb
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-06-13 14:31:45 -0300

    feat(Navigation): create auth navigation

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,11 +1,21 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Container } from './styles/global';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Go Barber!</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <Container>
+        <Routes />
+      </Container>
+    </NavigationContainer>
   );
 };
 
