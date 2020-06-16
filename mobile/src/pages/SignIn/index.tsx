434e31b510e11b966b347e37aa72ba5ce2d9678f
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-06-16 12:28:59 -0300

    feat(SignIn): finish layout

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,23 +1,62 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logo from '../../assets/logo.png';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccount,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
 
-      <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-email" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button onPress={() => console.log('eair')}>Entrar</Button>
-    </Container>
+            <Input name="email" icon="mail" placeholder="E-email" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => console.log('eair')}>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccount>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Crie uma conta</CreateAccountText>
+      </CreateAccount>
+    </>
   );
 };
 
