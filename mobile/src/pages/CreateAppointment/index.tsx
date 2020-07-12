d03a533d49c92eaf8ec0b92961d0970680aedd34
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-07-12 19:58:23 -0300

    feat(CreateAppointment): create header

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styled';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <Text>CreateAppointment</Text>;
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+
+  const { user } = useAuth();
+  const navigation = useNavigation();
+
+  const navigateToDashboard = useCallback(() => {
+    navigation.navigate('Dashboard');
+  }, [navigation]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateToDashboard}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
