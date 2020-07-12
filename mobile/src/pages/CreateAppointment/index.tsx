f6158de87e67f1f7c81cea577cf604791c37239f
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-07-12 20:33:08 -0300

    feat(CreateAppointment): list providers like Nubank's carousel style

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
+import client from '../../services/client';
+import ProviderDTO from '../Dashboard/dtos/ProviderDTO';
 
 import {
   Container,
@@ -10,6 +12,11 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styled';
 
 interface RouteParams {
@@ -18,14 +25,30 @@ interface RouteParams {
 
 const CreateAppointment: React.FC = () => {
   const route = useRoute();
-  const { providerId } = route.params as RouteParams;
+  const routeParams = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<ProviderDTO[]>([]);
+  const [currentProvider, setCurrentProvider] = useState(
+    routeParams.providerId,
+  );
 
   const { user } = useAuth();
   const navigation = useNavigation();
 
+  useEffect(() => {
+    client
+      .get<ProviderDTO[]>('/providers')
+      .then(res => setProviders(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   const navigateToDashboard = useCallback(() => {
     navigation.navigate('Dashboard');
   }, [navigation]);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setCurrentProvider(providerId);
+  }, []);
 
   return (
     <Container>
@@ -36,6 +59,26 @@ const CreateAppointment: React.FC = () => {
         <HeaderTitle>Cabeleireiros</HeaderTitle>
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          data={providers}
+          keyExtractor={p => p.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item: p }) => (
+            <ProviderContainer
+              isCurrent={p.id === currentProvider}
+              onPress={() => handleSelectProvider(p.id)}
+            >
+              <ProviderAvatar source={{ uri: p.avatar_url }} />
+              <ProviderName isCurrent={p.id === currentProvider}>
+                {p.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- mobile/src/pages/CreateAppointment/styled.ts
+++ mobile/src/pages/CreateAppointment/styled.ts
@@ -1,6 +1,16 @@
 import styled, { css } from 'styled-components/native';
 import { Platform } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList, RectButton } from 'react-native-gesture-handler';
+import ProviderDTO from '../Dashboard/dtos/ProviderDTO';
+
+interface ProviderContainerProps {
+  isCurrent: boolean;
+}
+
+interface ProviderNameProps {
+  isCurrent: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -33,4 +43,36 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(
+  FlatList as new () => FlatList<ProviderDTO>,
+)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background: ${props => (props.isCurrent ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  border-radius: 8px;
+  padding: 8px 12px;
+  margin-right: 16px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  color: ${props => (props.isCurrent ? '#232129' : '#f4ede8')};
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  margin-left: 16px;
 `;
